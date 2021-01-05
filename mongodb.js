91a3ceb19366a629696a2647410b03a0d14497e6
williamhsa <whsa2021>
2021-01-04 23:01:10 -0300

    feat: read from mongodb

--- mongodb.js
+++ mongodb.js
@@ -6,12 +6,12 @@
 
 const { MongoClient, ObjectID} = require('mongodb')
 
-const id = new ObjectID()
+/*const id = new ObjectID()
 console.log(id)
 console.log(id.id) //buffer com binarios
 console.log(id.getTimestamp())
 console.log(id.id.length) // tamanha original
-console.log(id.toHexString().length)
+console.log(id.toHexString().length)*/
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -24,7 +24,52 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
   console.log('Connected correctly!')
   const db = client.db(databaseName)
 
-  /*db.collection('users').insertOne({
+  // db.collection('users').findOne({ name: 'Isa', age: 1}, (error, user) => {
+  //   /*
+  //   * se no collection possui dados iguais, este metodo vai me retornar o primeiro.
+  //   * */
+  //   if (error) return console.log('Unable to fetch', error)
+  //   console.log(user)
+  //
+  // })
+
+  db.collection('users').findOne({ _id: new ObjectID('5fe9cfa0cb0fa44a94672c1d')}, (error, user) => {
+    /*
+    *  {_id: '5fe9cfa0cb0fa44a94672c1d'} procurar _id deste modo é errado
+    * */
+    if (error) return console.log('Unable to fetch', error)
+    console.log('findOne -> ', user)
+
+  })
+
+  // find return a Curso, que possui diferentes metodos.
+  db.collection('users').find({ age: 28}).toArray((error, users) => {
+    console.log(users); // me retona um array com todos os resultados encontrados dessa collection
+  })
+
+  db.collection('users').find({ age: 28}).count((error, count) => {
+    console.log(count);
+  })
+
+
+  // challenge time
+  db.collection('tasks').findOne({ _id: new ObjectID('5fea7b97469efc29792765ac')}, (error, task) => {
+    if (error) return console.log('Unable to fetch', error)
+    console.log('challenge 1 -> ', task)
+
+  })
+
+  db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+    console.log(tasks);
+  })
+
+})
+
+
+/*
+  INSERT DATA (COLLECTONS) ON MONGODB
+
+  db.collection('users').insertOne({
     _id: id,
     name: 'William',
     age: 28,
@@ -35,35 +80,34 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     console.log(result.ops);
   })*/
 
-  /*db.collection('users').insertMany([
-    {
-      name: 'Isa',
-      age: 26
-    },
-    {
-      name: 'Tupa',
-      age: 2
-    }
-  ], (error, result) => {
-    if (error) return console.log('Unable to insert documents!')
-    console.log(result.ops)
-  })*/
+/*db.collection('users').insertMany([
+  {
+    name: 'Isa',
+    age: 26
+  },
+  {
+    name: 'Tupa',
+    age: 2
+  }
+], (error, result) => {
+  if (error) return console.log('Unable to insert documents!')
+  console.log(result.ops)
+})*/
 
-  /*db.collection('tasks').insertMany([
-    {
-      description: 'task one',
-      completed: true
-    },
-    {
-      description: 'task two',
-      completed: false
-    },
-    {
-      description: 'task three',
-      completed: true
-    }
-  ], (err, result) => {
-    if (err) return console.log('Unable to insert tasks!')
-    console.log(result.ops)
-  })*/
-})
+/*db.collection('tasks').insertMany([
+  {
+    description: 'task one',
+    completed: true
+  },
+  {
+    description: 'task two',
+    completed: false
+  },
+  {
+    description: 'task three',
+    completed: true
+  }
+], (err, result) => {
+  if (err) return console.log('Unable to insert tasks!')
+  console.log(result.ops)
+})*/
